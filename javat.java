346d70c4bc72877eeb2239d45d3d4e9590ee99e5
Kunal Ghogale <127613404+9890430280@users.noreply.github.com>
2026-08-25 13:57:29 +0530

    Update javat.java (by kunal ghogale for update about pipeline)

--- javat.java
+++ javat.java
@@ -2,6 +2,7 @@
 class javat {
     public static void main(String[] args) {
         System.out.println("Here we are writing an java program for the creating docker image and container.");
+        System.out.println("Now pipeline working correct");
     }
 
     
